d8c765fd51d93fa007b1eb5819788f5a662a38b7
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2024-12-31 18:12:47 +0530

    init

---
+++ codechefr2/app.js
@@ -0,0 +1,23 @@
+//requires
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+//middlewares
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+
+app.listen(8080, () => {
+    console.log("server has started on port 8080");
+  });
+  
